23b108a1ea8b0e2d1dabf88e368fa94a586c3e71
Lukas Neugebauer <l.neugebauer@uke.de>
2022-11-25 17:34:05 +0100

    whatevs

--- plotting.py
+++ plotting.py
@@ -8,26 +8,41 @@ import matplotlib.patheffects as PathEffects
 import seaborn as sns
 from itertools import product
 import numpy as np
+from copy import deepcopy
+from collections import defaultdict
 
 
-def plot_goal_predictions(A, home_team, away_team, ax=None):
+def plot_goal_predictions(A, home_team, away_team, ax=None, odds=False):
     """
     Plot the posterior probability of exact outcomes
     """
+    odd_values = deepcopy(A)
+    odd_values = (1 / odd_values).round(2)
     if ax is None:
         fig, ax = plt.subplots(figsize=(10, 10), constrained_layout=True)
     sns.heatmap(A, cmap="magma", cbar=False, ax=ax)
     x_max, y_max = A.shape
     for x, y in product(range(x_max), range(y_max)):
-        txt = ax.text(
-            y + 0.5,
-            x + 0.5,
-            str(round(A[x, y] * 100, 2)) + " %",
-            ha="center",
-            va="center",
-            color="w",
-            size=20,
-        )
+        if odds:
+            txt = ax.text(
+                y + 0.5,
+                x + 0.5,
+                str(odd_values[x, y]),
+                ha="center",
+                va="center",
+                color="w",
+                size=20,
+            )
+        else:
+            txt = ax.text(
+                y + 0.5,
+                x + 0.5,
+                str(round(A[x, y] * 100, 2)) + " %",
+                ha="center",
+                va="center",
+                color="w",
+                size=20,
+            )
         txt.set_path_effects([PathEffects.withStroke(linewidth=5, foreground="k")])
     ax.set_aspect("equal")
     ax.set_xlabel(away_team, fontsize=30)
@@ -41,7 +56,7 @@ def plot_goal_predictions(A, home_team, away_team, ax=None):
         return fig
 
 
-def plot_outcome_prob(A, home_team, away_team, ax=None):
+def plot_outcome_prob(A, home_team, away_team, ax=None, odds=False):
     """
     Plot the winning and draw probabilities
     """
@@ -50,6 +65,7 @@ def plot_outcome_prob(A, home_team, away_team, ax=None):
     p_draw = np.diag(A).sum()
     x = np.arange(3)
     y = np.array([p_home, p_draw, p_away])
+    odd_values = (1 / y).round(2)
     if ax is None:
         ax = plt.axes()
     sns.barplot(x, y, hue=y, palette="magma", dodge=False, ax=ax)
@@ -58,20 +74,24 @@ def plot_outcome_prob(A, home_team, away_team, ax=None):
     ax.set_yticks([])
     ax.set_xticklabels([home_team, "Draw", away_team], fontsize=25)
     ax.set_title("Outcome probabilities", fontsize=30)
-    for i, p in enumerate(y):
-        ax.text(i, p, str(round(p * 100)) + " %", ha="center", va="bottom", fontsize=20)
+    for i, (p, o) in enumerate(zip(y, odd_values)):
+        if odds:
+            ax.text(i, p, str(o), ha="center", va="bottom", fontsize=20)
+        else:
+            ax.text(i, p, str(round(p * 100)) + " %", ha="center", va="bottom", fontsize=20)
     ax.spines["top"].set_visible(False)
     ax.spines["right"].set_visible(False)
     ax.spines["left"].set_visible(False)
 
 
-def plot_diff_prob(A, home_team, away_team, ax=None):
+def plot_diff_prob(A, home_team, away_team, ax=None, odds=False):
     """
     Plot probability of difference between home and away goals
     """
     diffs = {x: np.diag(A.T, x).sum() for x in np.arange(-4, 5)}
     x = list(diffs.keys())
     y = list(diffs.values())
+    odd_values = (1 / np.array(y)).round(2)
     if ax is None:
         ax = plt.axes()
     g = sns.barplot(x=x, y=y, hue=y, palette="magma", dodge=False, ax=ax)
@@ -82,18 +102,58 @@ def plot_diff_prob(A, home_team, away_team, ax=None):
     ax.set_title(f"P of {home_team} - {away_team} goal differences", fontsize=30)
     for l in ["left", "right", "top"]:
         ax.spines[l].set_visible(False)
-    for i, p in enumerate(y):
-        ax.text(i, p, str(round(p * 100)) + " %", ha="center", va="bottom", fontsize=20)
+    for i, (p, o) in enumerate(zip(y, odd_values)):
+        if odds:
+            ax.text(i, p, str(o), ha="center", va="bottom", fontsize=20)
+        else:
+            ax.text(i, p, str(round(p * 100)) + " %", ha="center", va="bottom", fontsize=20)
 
+        
+def plot_total_score(A, cutoff=10, ax=None, odds=False):
+    if ax is None:
+        ax = plt.axes()
+    outcome_sum = defaultdict(int)
+    for i in range(A.shape[0]):
+        for j in range(A.shape[1]):
+            outcome_sum[i + j] += A[i, j]
+    x = list(outcome_sum.keys())[:10]
+    y = list(outcome_sum.values())[:10]
+    y = np.cumsum(y)
+    odd_values = (1 / y).round(2)
+    sns.barplot(x=x, y=y, hue=y,palette='magma', dodge=False, ax=ax)
+    ax.legend([], [], frameon=False)
+    for xx, yy, o in zip(x, y, odd_values):
+        if odds:
+            ax.text(xx, yy, str(o), va='bottom', ha='center', fontsize=20)
+        else:
+            ax.text(xx, yy, str(round(yy, 2)), va='bottom', ha='center', fontsize=20)
+    ax.set_yticks([])
+    ax.set_xticklabels(x, fontsize=25)
+    ax.set_title('P total scores', fontsize=30)
+    for side in ['top', 'right', 'left']:
+        ax.spines[side].set_visible(False)
 
-def plot_full_predictions(A, home_team, away_team, cutoff=5):
+        
+def plot_full_predictions(A, home_team, away_team, cutoff=5, odds=False, axs=None):
     """
     Do all of the above at once
     """
-    fig, axs = plt.subplots(ncols=3, nrows=1, figsize=(30, 10), constrained_layout=True)
+    return_fig = False
+    if axs is None:
+        fig, axs = plt.subplots(ncols=4, nrows=1, figsize=(30, 10), constrained_layout=True)
+        return_fig = True
     plot_goal_predictions(
-        A[: cutoff + 1, : cutoff + 1], home_team, away_team, ax=axs[0]
+        A[: cutoff + 1, : cutoff + 1], home_team, away_team, ax=axs[0], odds=odds
     )
-    plot_outcome_prob(A, home_team, away_team, ax=axs[1])
-    plot_diff_prob(A, home_team, away_team, ax=axs[2])
+    plot_outcome_prob(A, home_team, away_team, ax=axs[1], odds=odds)
+    plot_diff_prob(A, home_team, away_team, ax=axs[2], odds=odds)
+    plot_total_score(A, ax=axs[3], odds=odds)
+    if return_fig: 
+        return fig
+
+def plot_p_and_odds(**kwargs):
+    fig, axs = plt.subplots(ncols=4, nrows=2, figsize=(30, 20), constrained_layout=True)
+    plot_full_predictions(**kwargs, odds=False, axs=axs[0])
+    plot_full_predictions(**kwargs, odds=True, axs=axs[1])
     return fig
+    
---
+++ stan/dixon_coles_td_exact.stan
@@ -0,0 +1,97 @@
+// Dixon Coles model, including temporal decay, no sum-to-0 constraint
+// Uses the exact outcome for the likelihood function
+// http://web.math.ku.dk/~rolf/teaching/thesis/DixonColes.pdf
+
+functions{
+  // compute the correction term
+  real log_tau_correction(int x, int y, real rho, real rate_home, real rate_away){
+    real tau = 1;
+    if (x == 0 && y == 0){
+     tau = tau - rate_home * rate_away * rho;
+    }else if(x == 0 && y == 1){
+      tau = tau + rate_home * rho;
+    }else if(x == 1 && y == 0){
+      tau = tau + rate_away * rho;
+    }else if(x == 1 && y == 1){
+      tau = tau - rho;
+    }
+    return log(tau);
+  }
+
+  //custom pmf including correction and temporal decay.
+  real dixon_coles_lpmf(int[] goals, int time, real rho, real decay, real log_rate_home, real log_rate_away){
+    int home_goals = goals[1];
+    int away_goals = goals[2];
+
+    real logp_home = poisson_log_lpmf(home_goals | log_rate_home);
+    real logp_away = poisson_log_lpmf(away_goals | log_rate_away);
+    real log_tau = log_tau_correction(home_goals, away_goals, rho, exp(log_rate_home), exp(log_rate_away));
+    return exp(-decay * time) * (logp_home + logp_away + log_tau);
+  }
+}
+
+data{
+  int n_teams;
+  int n_matches;
+  int home_team[n_matches];
+  int away_team[n_matches];
+  int home_goals[n_matches];
+  int away_goals[n_matches];
+  // how many days has it been since the game
+  int time[n_matches];
+  // indicate whether the game was on neutral ground
+  // it should really be int[] of 1 and 0, but stan doesn't allow real * int[]
+  vector[n_matches] is_home;
+}
+
+parameters{
+  real home_advantage; //factoring in that teams do better at home
+  real intercept;
+  vector[n_teams] attack;
+  vector[n_teams] defense;
+  //constraint on rho enforced in transformed parameters block, cmp p. 270 in the paper
+  real<lower=0, upper=1> rho_raw;
+  real<lower=0> decay;
+}
+
+transformed parameters{
+  //enforce constraint on rho by sampling on the range [0, 1] and shifting into the boundaries
+  //lower bound: max( - 1/lambda, -1/mu)
+  //upper bound: min(1/lambda * mu, 1)
+  real rho;
+  {
+    vector[n_matches] log_rate_home = (
+      intercept + attack[home_team] - defense[away_team] + (home_advantage * is_home)
+    );
+    vector[n_matches] log_rate_away = (
+      intercept + attack[away_team] - defense[home_team]
+    );
+    real upper_bound = min({min(1. ./ exp(log_rate_home + log_rate_away)), 1});
+    real lower_bound = max({max(-1. ./ exp(log_rate_home)), max(-1. ./ exp(log_rate_away))});
+    real range = upper_bound - lower_bound;
+    rho = lower_bound + rho_raw * range;
+  }
+}
+
+model{
+  vector[n_matches] log_rate_home = (
+    intercept + attack[home_team] - defense[away_team] + (home_advantage * is_home)
+  );
+  vector[n_matches] log_rate_away = (
+    intercept + attack[away_team] - defense[home_team]
+  );
+
+  home_advantage ~ normal(0, 3);
+  intercept ~ normal(0, 3);
+  attack ~ std_normal();
+  defense ~ std_normal();
+  rho_raw ~ uniform(0, 1);
+  decay ~ normal(0, 1);
+
+  // update model with log likelihood of data
+  for (m in 1:n_matches){
+    int goals[2] = {home_goals[m], away_goals[m]};
+    target += dixon_coles_lpmf(goals | time[m], rho, decay, log_rate_home[m], log_rate_away[m]);
+  }
+
+}
